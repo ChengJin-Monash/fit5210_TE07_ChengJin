614db16a29a488e2e731192b8b658b55323d3028
ChengJin-Monash <cjin0018@student.monash.edu>
2025-05-02 14:17:16 +1000

    update for iteration 2, mainly on chatbot function

---
+++ tinyllama1b/python_proj/cmdlog.sh
@@ -0,0 +1,13 @@
+# ~/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install python3-pip uvicorn -y
+
+# for cv_builder
+cd ~/tinyllama1b/python_proj/cv_builder
+
+
+
+
+# for chatbot
+cd ~/tinyllama1b/python_proj/chatbot
